ccd1865b543fa80aba47566adf05eeba5b25eec8
Chils Missy <missyforevah@example.com>
2025-06-02 12:49:53 +0800

    initial commit

---
+++ src/lms/title_index.php
@@ -0,0 +1,39 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/Logo.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">Laguna State Polythecnic University</p>
+							<h3>
+
+							<p>E - Learning</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+									<h1>Collage of</h1>
+									<h1>Computer</h1>
+									<h1>Studies</h1>
+												
+								</div>		
+						</div>		
+				</div>
